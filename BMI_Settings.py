ef5575a4d0bef2cd595663626f888aff62495035
gir8 <137758189+gir8@users.noreply.github.com>
2023-10-23 12:15:26 -0400

    Create BMI_Settings.py
    Added settings python file.

---
+++ BMI_Settings.py
@@ -0,0 +1,22 @@
+#!/user/bin/env python3
+"""This file contains settings to be used in the BMI_Calc python file."""
+
+# Font sizes
+FONT = "Times New Roman"
+MAIN_TEXT_SIZE = 150
+INPUT_FONT_SIZE = 26
+SMALL_INPUT_FONT_SIZE = 17
+SWITCH_FONT_SIZE = 20
+CORNER_RADIUS = 7
+
+# Colors
+PINK = "#FF69B4"
+LIGHT_PINK = "#FFBFFF"
+DARK_PINK = "#ED007B"
+WHITE = "#F7F7F7"
+BLACK = "#170229"
+LIGHT_GRAY = "#F8F8F8"
+GRAY = "#C7C7C7"
+
+# Title Hex
+TITLE_HEX_COLOR = 0x00EFEFFF  # hex order: 0x00bbggrr #50BFAB
